b6784be1ed7ad00bf5e25f64f3a24af199a0a309
PatriciaSouza <patriciasdr@hotmail.com>
2025-11-04 18:06:47 -0300

    atividade Collection Set e Array

---
+++ atividade4/atividadeCollectionArray.ts
@@ -0,0 +1,20 @@
+import readline from 'readline-sync';
+
+const cores: Array<string> = new Array<string>();
+
+for (let i = 1; i <= 5; i++) {
+  let cor = readline.question(`Digite a ${i}ª cor: `);
+  cores.push(cor);
+}
+
+console.log("\nListar todas as cores:");
+for (let cor of cores) {
+  console.log(cor);
+}
+
+cores.sort();
+
+console.log("\nOrdenar as cores:");
+for (let cor of cores) {
+  console.log(cor);
+}
---
+++ atividade4/atividadeCollectionSet.ts
@@ -0,0 +1,15 @@
+import readline from 'readline-sync';
+
+const numeros: Set<number> = new Set<number>();
+
+for (let i = 1; i <= 10; i++) {
+    let numero = readline.questionInt(`Digite o ${i}º número: `);
+    numeros.add(numero);
+}
+
+const numerosOrdenados = Array.from(numeros).sort((a, b) => a - b);
+
+console.log("\nListar dados do Set:");
+for (let numero of numerosOrdenados) {
+    console.log(numero);
+}
