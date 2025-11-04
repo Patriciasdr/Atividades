548746f8c9fda68d4e7ea1344d9ce53485dc37ea
PatriciaSouza <patriciasdr@hotmail.com>
2025-11-04 18:52:07 -0300

    Corrige tsconfig para esModuleInterop e compatibilidade com readline-sync

--- atividade4/atividadeCollectionSet.ts
+++ atividade4/atividadeCollectionSet.ts
@@ -1,14 +1,18 @@
 import readline from 'readline-sync';
 
+// Cria o Set do tipo number
 const numeros: Set<number> = new Set<number>();
 
+// Entrada de dados
 for (let i = 1; i <= 10; i++) {
     let numero = readline.questionInt(`Digite o ${i}º número: `);
-    numeros.add(numero);
+    numeros.add(numero); // Adiciona o número no Set (sem repetir)
 }
 
+// Converte o Set para Array e ordena em ordem crescente
 const numerosOrdenados = Array.from(numeros).sort((a, b) => a - b);
 
+// Saída de dados
 console.log("\nListar dados do Set:");
 for (let numero of numerosOrdenados) {
     console.log(numero);
